478b7136e10b815cfd3fc8e13f8cdeeb73239ec4
Cappu123 <cappyalemayehu12@gmail.com>
2024-02-10 02:14:13 -0800

    on the engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,4 +25,31 @@ class FileStorage:
         """
         return self.__objects
     
-    def
+    def new(self, object):
+        """
+        Sets in __objects the obj with key <obj class name>.id
+         (ex: to store a BaseModel object with id=12121212, the key will be a       seModel.12121212)
+         Args:
+            object(obj): object to write
+        """
+        self.__objects[object.__class__.__name__ + '.' + str(object)] = object
+    def save(self):
+        """
+        serializes __objects to the JSON file
+        (path: __file_path)
+        """
+        with open(self.file_path, 'w+') as f:
+            json.dump({key: value.to_dect() for key, value in self__objects.items()}, f)
+             def reload(self):
+        """
+        deserializes the JSON file to __objects, if the JSON
+        file exists, otherwise nothing happens)
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                dict = json.loads(f.read())
+                for value in dict.values():
+                    cls = value["__class__"]
+                    self.new(eval(cls)(**value))
+        except Exception:
+            pass
